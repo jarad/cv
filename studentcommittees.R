7159b6590159f8c4fd1c0002eeac6b82cdc3053f
Jarad Niemi <niemi@iastate.edu>
2013-10-18 10:05:34 -0500

    added studentcommittees.csv

---
+++ studentcommittees.R
@@ -0,0 +1,14 @@
+library(xtable)
+
+d = read.csv("studentcommittees.csv")
+
+d = d[order(d$Completed, d$Degree, d$Department, decreasing=TRUE),]
+
+tab = xtable(d[d$School=="ISU", ])
+print(tab, file="ISUstudentcommittees.tex", include.rownames=FALSE)
+
+
+tab = xtable(d[d$School=="UCSB", ])
+print(tab, file="UCSBstudentcommittees.tex", include.rownames=FALSE)
+
+
