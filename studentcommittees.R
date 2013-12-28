d3ec674fbe8d3944d4d0b193873bb862c2448621
Jarad Niemi <niemi@iastate.edu>
2013-12-28 09:36:02 -0600

    updated grad student committees

--- studentcommittees.R
+++ studentcommittees.R
@@ -2,14 +2,30 @@ library(xtable)
 
 d = read.csv("studentcommittees.csv")
 
-d = d[order(d$Completed, d$Degree, d$Department, decreasing=TRUE),]
+d = d[order(d$School, d$Completed, d$Degree, d$Department, decreasing=TRUE),]
 
-tab = xtable(d[d$School=="ISU", ])
-print(tab, file="ISUstudentcommittees.tex", include.rownames=FALSE)
+#tab = xtable(d[d$School=="ISU", ], 
+#      caption="Graduate student committees at ISU",
+#      label="isustudentcommittees")
+#print(tab, file="ISUstudentcommittees.tex",
+#      include.rownames=FALSE,
+#      table.placement="h")
 
 
-tab = xtable(d[d$School=="UCSB", ])
-print(tab, file="UCSBstudentcommittees.tex", include.rownames=FALSE)
+#tab = xtable(d[d$School=="UCSB", ], 
+#      caption="Graduate student committees at UCSB",
+#      label="ucsbstudentcommittees")
+#print(tab, file="UCSBstudentcommittees.tex",
+#      include.rownames=FALSE,
+#      table.placement="h")
+
+
+tab = xtable(d, 
+      caption="Graduate student committees",
+      label="tab:studentcommittees")
+print(tab, file="studentcommittees.tex", 
+      include.rownames=FALSE,
+      table.placement="h")
 
 
 # ISU non-STAT committees
